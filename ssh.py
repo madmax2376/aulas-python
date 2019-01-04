405c1c7a268d759eb5d47b43d3e99127b58ed9d0
madmax2376 <mauricio@glink.net.br>
2019-01-04 10:08:42 -0200

    Update

---
+++ ssh.py
@@ -0,0 +1,22 @@
+import paramiko
+from paramiko import SSHClient
+
+
+class SSH:
+    def __init__(self):
+        self.ssh = SSHClient()
+        self.ssh.load_system_host_keys()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(
+            hostname='xxx',
+            username='xxx', password='xxx')
+
+    def exec_cmd(self, cmd):
+        stdin, stdout, stderr = self.ssh.exec_command(cmd)
+        if stderr.channel.recv_exit_status() != 0:
+            print(stderr.read())
+        else:
+            print(stdout.read())
+if __name__ == '__main__':
+    ssh = SSH()
+    ssh.exec_cmd("xxx")
