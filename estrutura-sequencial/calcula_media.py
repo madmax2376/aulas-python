d70090bb2b2091e03c10a7c1a1e127241e70567d
madmax2376 <mauricio@glink.net.br>
2019-05-13 18:19:39 -0300

    update

---
+++ estrutura-sequencial/calcula_media.py
@@ -0,0 +1,8 @@
+print('**********ESSE PROGRAMA CALCULA A MÉDIA DE IDADE FAMILIAR**********')
+i1 = int(input('Por favor, digite a idade do seu pai: '))
+i2 = int(input('Por favor, digite a idade da sua mae: '))
+i3 = int(input('Por favor, digite a idade do seu irmao: '))
+i4 = int(input('Por favor, digite a sua idade: '))
+media = (i1 +i2 +i3 +i4) / 4
+print('A idade media da sua familia e: ', media, 'anos')
+print('**********FIM**********')
