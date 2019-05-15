b4bc29a2f05c06bb64556afe465e6bd85b03c59b
madmax2376 <mauricio@glink.net.br>
2019-05-14 23:24:52 -0300

    update

---
+++ operacoes-numericas/numeros.py
@@ -0,0 +1,93 @@
+# Operações Básicas com Números
+import numpy
+print ("Operações Básicas com Números")
+
+# Soma
+a = 2 + 2
+print("Soma de um número")
+print (a)
+
+# Subtração
+b = 4 - 3
+print("Subtração de um Número")
+print(b)
+
+# Multiplicação
+c = 3 * 3
+print("Produto entre dois números")
+print(c)
+
+# Potência
+d = 4 ** 2
+print("Resultado de uma Potenciação")
+print(d)
+
+# Módulo
+e = 10 % 3
+print("Resultado de um Módulo")
+print(e)
+
+# Divisão com Resultado Float
+f = 4 / 2
+print("Divisão com Resultado Float")
+print(f)
+
+# Divisão com Resultado Int
+g = 4 // 2
+print("Divisão com Resultado Inteiro")
+print(g)
+
+# Função (Type)
+type(5)
+type(5.0)
+type("I am a string")
+
+# Conversão Float e Int
+decimal1 = float(9)
+print ("Abaixo o Resultado de uma conversão float")
+print(decimal1)
+
+decimal2 = float(23)
+print ("Abaixo o Resultado de uma conversão float")
+print(decimal2)
+
+inteiro1 = int(3.0)
+print ("Abaixo o Resultado de uma conversão int")
+print(inteiro1)
+
+inteiro2 = int(3.5)
+print ("Abaixo o Resultado de uma conversão int")
+print(inteiro2)
+
+# Conversão Hexadecimal
+hexa1 = hex(394)
+print ("Abaixo o Resultado de uma conversão hexadecimal")
+print(hexa1)
+
+hexa2 = hex(217)
+print ("Abaixo o Resultado de uma conversão hexadecimal")
+print(hexa2)
+
+binario1 = bin(286)
+print ("Abaixo o Resultado de uma conversão binario")
+print(binario1)
+
+binario2 = bin(390)
+print ("Abaixo o Resultado de uma conversão binario")
+print(binario2)
+
+# Funções abs, round e pow
+# Retorna o valor absoluto
+absoluto1 = abs(-8)
+print("Abaixo o Valor Absoluto de um Número")
+print(absoluto1)
+
+# Retorna o valor com arredondamento
+arred = round(3.14151922,2)
+print("Abaixo o Arredondamento de um Número")
+print(arred)
+
+# Potência
+pot = pow(4,2)
+print("Abaixo a Potenciação de um Número")
+print(pot)
